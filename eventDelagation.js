cceb2bb1772044fd97b6d8c6a6654854b15d33ea
jatinmishra1 <balajimahraaj123@gmail.com>
2023-09-11 22:48:48 +0530

    event-delegation

--- eventDelagation.js
+++ eventDelagation.js
@@ -1,1 +1,21 @@
 //to handel event listeners
+
+// so  basic idea of event delegation is to improve performance
+//so idea is that despite of adding avent listener of every element inside parent, add event listener to parent only
+//and it will do the working with the help of bubbling and capturing 
+<div id="category">
+    <ul>
+        <li id="laptop">laptop</li>
+        <li id="camera">camera</li>
+        <li id="phones">phones</li>
+        <li id="mobile">mobile</li>
+    </ul>
+</div>
+document.querySelector("#category").
+addEventListener("click",(e)=>{
+    if(e.target.tagName==='LI'){
+        window.location.href="/"+e.target.id
+    }
+console.log(e.target.id)
+
+})
