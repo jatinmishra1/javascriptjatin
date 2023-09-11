b169b557bb170d6db01e72a3304a48030f97bbaa
jatinmishra1 <balajimahraaj123@gmail.com>
2023-09-11 17:08:30 +0530

    currying and closure concept added

---
+++ currying.js
@@ -0,0 +1,20 @@
+// let multiply=function(x,y){
+//     console.log(x*y);
+// }
+// let multiplebytwo=multiply.bind(this,2)
+// multiplebytwo(5);
+
+// let multiplebythree=multiply.bind(this,3);
+// multiplebythree(5)
+
+
+
+//we can do with the help of closures also
+let multiply=function(x){
+    return function(y){
+        console.log(x*y);
+    }
+}
+
+let multiplebythree=multiply(2);
+multiplebythree(3);
