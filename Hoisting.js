9eb0943e5899d60da420baccdd62270e254dfabd
jatinmishra1 <balajimahraaj123@gmail.com>
2023-09-16 11:45:14 +0530

    hosting

--- Hoisting.js
+++ Hoisting.js
@@ -5,3 +5,6 @@ var  x=10;
 function getName (){
     console.log("my name is mishra ji")
 }
+
+
+//so hoisting is the phenomenon in the js by using which you can access the variables even before initializing it and will not get an error
