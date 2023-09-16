6574b0d6b6727b2f1e56bd2be6c4622f26dc77bd
jatinmishra1 <balajimahraaj123@gmail.com>
2023-09-16 16:17:46 +0530

    firstclass functions
    c

---
+++ firstClassFunctions.js
@@ -0,0 +1,37 @@
+//function statement or function declaration
+function a(){
+
+}
+
+//function expresssion
+const b=function b(){
+    console.log("hellos eema")
+}
+
+//anonymous function
+
+// function(){
+
+// }
+//but this will give error
+//so anonymous function are used when functions are used as value
+//like 
+const c=function(){
+
+}
+
+
+//named function expression
+
+const dd=function b(){
+
+}
+
+
+//first class functions
+//the ability to use  function as values is knows as first class functions
+//that is you can pass argumnest as function and you can receive parameters as functions
+//and even you can return as functions in js
+
+//functions are first class citizena ----so ability to use functions as values thats  why they are also know ans first class citizens
+
