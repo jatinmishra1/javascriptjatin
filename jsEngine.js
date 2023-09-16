3ad221abe5c45a2344984387cccce94e0c116f95
jatinmishra1 <balajimahraaj123@gmail.com>
2023-09-16 16:29:10 +0530

    js engine

---
+++ jsEngine.js
@@ -0,0 +1,11 @@
+//js engine works in three stages
+//parsing--->compilation--->execution
+//durign this parsing this code is broken into tokens
+//and there is also known as synatx parser
+//ans the job of syntax parser is to convert take this code and covert this into AST---i.e Abstract Syntax Tree
+
+
+//then comes compilation and execution stage
+//there are two things interpreter and compiler
+//so js and used both as interpreter and copiler and thats why it is called as just in time cpilation laguage and it all depends on js engine
+//how to use ,js can also be used only as compiler or interepreter or both
